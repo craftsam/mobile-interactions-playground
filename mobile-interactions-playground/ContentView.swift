52ffa229512f1d591702fd8f4a22e71fe093b576
Sam Townsend <sam@samt.io>
2026-03-27 11:12:31 -0700

    Initial Commit

---
+++ mobile-interactions-playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobile-interactions-playground
+//
+//  Created by Sam Townsend on 2026-03-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile-interactions-playground/mobile_interactions_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_interactions_playgroundApp.swift
+//  mobile-interactions-playground
+//
+//  Created by Sam Townsend on 2026-03-27.
+//
+
+import SwiftUI
+
+@main
+struct mobile_interactions_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
